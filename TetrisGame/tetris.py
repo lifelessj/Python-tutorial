713f9bbdd9937d3c6d0762785505a1642d328cda
lifelessj <kjjovans@gmail.com>
2026-01-07 19:14:19 +0300

    tetris rules1

--- TetrisGame/tetris.py
+++ TetrisGame/tetris.py
@@ -41,3 +41,54 @@ SHAPES = [
         [".....", ".....", ".OOO.", ".O...", "....."],
     ],
 ]
+
+class Tetromino:
+    def __init__(self, x, y, shape):
+        self.x = x
+        self.y = y
+        self.shape = shape
+        self.color = random.choice(
+            COLORS
+        )  # You can choose different colors for each shape
+        self.rotation = 0
+
+
+class Tetris:
+    def __init__(self, width, height):
+        self.width = width
+        self.height = height
+        self.grid = [[0 for _ in range(width)] for _ in range(height)]
+        self.current_piece = self.new_piece()
+        self.game_over = False
+        self.score = 0  # Add score attribute
+
+    def new_piece(self):
+        # Choose a random shape
+        shape = random.choice(SHAPES)
+        # Return a new Tetromino object
+        return Tetromino(self.width // 2, 0, shape)
+
+    def valid_move(self, piece, x, y, rotation):
+        """Check if the piece can move to the given position"""
+        for i, row in enumerate(
+            piece.shape[(piece.rotation + rotation) % len(piece.shape)]
+        ):
+            for j, cell in enumerate(row):
+                try:
+                    if cell == "O" and (
+                        self.grid[piece.y + i + y][piece.x + j + x] != 0
+                    ):
+                        return False
+                except IndexError:
+                    return False
+        return True
+
+    def clear_lines(self):
+        """Clear the lines that are full and return the number of cleared lines"""
+        lines_cleared = 0
+        for i, row in enumerate(self.grid[:-1]):
+            if all(cell != 0 for cell in row):
+                lines_cleared += 1
+                del self.grid[i]
+                self.grid.insert(0, [0 for _ in range(self.width)])
+        return lines_cleared
