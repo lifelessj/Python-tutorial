dbea84ef0a2baaa366c9526c5fae9d715fb4ccc4
lifelessj <kjjovans@gmail.com>
2026-01-10 23:02:32 +0300

    jokesgame3

--- Joker/jokes.py
+++ Joker/jokes.py
@@ -13,3 +13,17 @@ while tellajoke:
     if ans == "y":
         x = requests.get("https://v2.jokeapi.dev/joke/" + category + "?lang=" + lang)
         x = x.json()
+
+        if "joke" in x:
+            print("\n" + x["joke"])
+        else:
+            print("\n" + x["setup"])
+            print("...")
+            time.sleep(1)
+            print(x["delivery"])
+    elif ans == "s":
+        s_ans = str(
+            input(
+                "Which settings would you like to edit? (c => category, l => language): "
+            )
+        )        
