8542fdde936a88ef94987f43ba3464917d3279b4
lifelessj <kjjovans@gmail.com>
2026-01-10 23:01:34 +0300

    jokesgame2

--- Joker/jokes.py
+++ Joker/jokes.py
@@ -1,2 +1,15 @@
 import requests
 import time
+
+tellajoke = True
+
+category = "Any"
+lang = "eng"
+while tellajoke:
+    ans = str(
+        input("\n\nDo you want to read a joke?(y => yes; n => exit; s=> settings): ")
+    )
+
+    if ans == "y":
+        x = requests.get("https://v2.jokeapi.dev/joke/" + category + "?lang=" + lang)
+        x = x.json()
