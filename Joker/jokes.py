871ee66bc97572babc31157ba4b59870979d15ea
lifelessj <kjjovans@gmail.com>
2026-01-10 23:03:53 +0300

    jokesdone

--- Joker/jokes.py
+++ Joker/jokes.py
@@ -27,3 +27,38 @@ while tellajoke:
                 "Which settings would you like to edit? (c => category, l => language): "
             )
         )        
+        if s_ans == "c":
+            print("Current selected category: " + category + "\n")
+            c_ans = str(
+                input(
+                    "Selectable categories:\n a => Any\n p => Programming\n m => Misc\n d => dark\n s => "
+                    "Spooky\n c => Christmas\n > "
+                )
+            )
+            if c_ans == "p":
+                category = "Programming"
+            elif c_ans == "m":
+                category = "Misc"
+            elif c_ans == "d":
+                category = "Dark"
+            elif c_ans == "s":
+                category = "Spooky"
+            elif c_ans == "c":
+                category = "Christmas"
+            elif c_ans == "a":
+                category = "Any"
+            print("Category " + category + " set!")
+        elif s_ans == "l":
+            print("Current selected language: " + lang)
+            l_ans = str(
+                input(
+                    "Selectable languages:\n en => English\n cs => Czech\n de => German\n es => Spanish\n fr => "
+                    "French\n pt => Portuguese\n > "
+                )
+            )
+            if l_ans in ["en", "cs", "de", "es", "fr", "pt"]:
+                lang = l_ans
+                print("Language " + lang + " set!\n")
+
+    else:
+        tellajoke = False
